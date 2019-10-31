ccb841019b9043d991837eef583be5a2b480101c
ramadis <rramiro.o@hotmail.com>
2019-10-31 18:03:49 -0300

    Fix scripts error messages

--- client/src/main/assembly/overlay/query1.sh
+++ client/src/main/assembly/overlay/query1.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
-for param in $@
-do
-  if [[ $param == -DoutPath=* ]];
-  then
-    SUBSTRING=$(echo $param| cut -d'=' -f 2)
-    java $* -cp 'lib/jars/*' "pod.client.Query1Client" > "${SUBSTRING}query1.txt"
-  fi
-done
+if [[ $* == *-DoutPath=* ]];
+then
+  for param in $@
+  do
+    if [[ $param == -DoutPath=* ]];
+    then
+      SUBSTRING=$(echo $param| cut -d'=' -f 2)
+      java $* -cp 'lib/jars/*' "pod.client.Query1Client" > "${SUBSTRING}query1.txt"
+    fi
+  done
+else
+  java $* -cp 'lib/jars/*' "pod.client.Query1Client"
+fi
+
